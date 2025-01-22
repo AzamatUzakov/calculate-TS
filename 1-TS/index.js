be97ca6745120909832e1d13ea6d8a85ba806543
AzamatUzakov <azamatakmal053@gmail.com>
2025-01-22 18:18:21 +0500

    ts-colculate in progress

--- 1-TS/index.js
+++ 1-TS/index.js
@@ -1,1 +1,7 @@
 "use strict";
+let plus = document.querySelector('#plus');
+let minus = document.querySelector('#minus');
+let times = document.querySelector('#times');
+let divide = document.querySelector('#divide');
+times = 234;
+console.log(plus, minus, times, divide);
---
+++ 1-TS/index.ts
@@ -0,0 +1,17 @@
+
+
+
+let plus = document.querySelector<HTMLInputElement>('#plus')
+let minus = document.querySelector<HTMLInputElement>('#minus')
+let times = document.querySelector<HTMLInputElement>('#times')
+let divide = document.querySelector<HTMLInputElement>('#divide')
+
+let total = document.querySelector<HTMLInputElement>(".total")
+
+let inp1 = document.querySelector<HTMLInputElement>(".num1")
+let inp2 = document.querySelector<HTMLInputElement>(".num2")
+
+
+
+
+
