3e994c1910aba0110da795f865c3cb3606643771
AzamatUzakov <azamatakmal053@gmail.com>
2025-01-23 16:03:17 +0500

    finish colculate

--- 1-TS/index.js
+++ 1-TS/index.js
@@ -3,5 +3,47 @@ let plus = document.querySelector('#plus');
 let minus = document.querySelector('#minus');
 let times = document.querySelector('#times');
 let divide = document.querySelector('#divide');
-times = 234;
-console.log(plus, minus, times, divide);
+let resultBtn = document.querySelector('.result');
+let total = document.querySelector(".total");
+let inp1 = document.querySelector(".num1");
+let inp2 = document.querySelector(".num2");
+let operator = "+";
+let operatorBtns = document.querySelectorAll("#operator_btns button");
+if (operatorBtns) {
+    operatorBtns.forEach(btn => {
+        btn.onclick = () => {
+            operator = btn.value;
+        };
+    });
+}
+if (resultBtn) {
+    resultBtn.onclick = () => {
+        if (inp1 && inp2 && total) {
+            let num1 = parseFloat(inp1.value);
+            let num2 = parseFloat(inp2.value);
+            let result = 0;
+            switch (operator) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "x":
+                    result = num1 * num2;
+                    break;
+                case ":":
+                    if (num2 === 0) {
+                        total.innerHTML = "";
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    total.innerText = "Error2";
+            }
+            total.innerHTML = result.toString();
+        }
+    };
+}
+console.log(operatorBtns);
+console.log(operator);
--- 1-TS/index.ts
+++ 1-TS/index.ts
@@ -6,22 +6,75 @@ let minus = document.querySelector<HTMLInputElement>('#minus')
 let times = document.querySelector<HTMLInputElement>('#times')
 let divide = document.querySelector<HTMLInputElement>('#divide')
 
+let resultBtn = document.querySelector<HTMLButtonElement>('.result')
 let total = document.querySelector<HTMLInputElement>(".total")
-
 let inp1 = document.querySelector<HTMLInputElement>(".num1")
 let inp2 = document.querySelector<HTMLInputElement>(".num2")
 
+let operator: string = "+"
+let operatorBtns = document.querySelectorAll<HTMLButtonElement>("#operator_btns button")
 
 
 
-if (inp1 && inp2 && total) {
-    const num1: number = parseFloat(inp1.value)
-    const num2: number = parseFloat(inp2.value)
-
-    let result: number = 0
-
-    switch(
-        
-    )
+if (operatorBtns) {
+    operatorBtns.forEach(btn => {
+        btn.onclick = () => {
+            operator = btn.value
+        }
+    });
 }
+
+if (resultBtn) {
+    resultBtn.onclick = () => {
+
+        if (inp1 && inp2 && total) {
+            let num1 = parseFloat(inp1.value)
+            let num2 = parseFloat(inp2.value)
+
+            let result: number = 0
+
+
+            switch (operator) {
+                case "+":
+                    result = num1 + num2
+                    break;
+
+
+                case "-":
+                    result = num1 - num2
+                    break;
+
+                case "x":
+                    result = num1 * num2
+                    break;
+
+
+                case ":":
+                    if (num2 === 0) {
+                        total.innerHTML = ""
+                    }
+                    result = num1 / num2
+                    break;
+
+                default:
+                    total.innerText = "Error2"
+            }
+            total.innerHTML = result.toString()
+        }
+
+    }
+
+
+
+}
+
+
+
+
+console.log(operatorBtns);
+
+
+
+console.log(operator);
+
 
